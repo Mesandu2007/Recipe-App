6083c474e00a2680aaf4429f12e61838950226aa
Mesandu Gunarwardhana <mesandugunarwardhana@gmail.com>
2025-12-03 22:44:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Recipe from "./components/recipe"
+import Grid from "./components/Grid"
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Recipe/>
+      
+    </div>
+  )
+}
+
+export default App
